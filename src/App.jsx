4e8ea4bbcb17af9ad2df3e3024a92d18da520a4c
uniqweber <uniqwebersocial@gmail.com>
2025-04-02 00:38:27 +0600

    folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600 h-screen flex items-center justify-center"> Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
