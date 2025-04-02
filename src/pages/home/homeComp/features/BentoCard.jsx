ffedfe23d8f5ffd432705f2f9ff7ef5e05bb04aa
uniqweber <uniqwebersocial@gmail.com>
2025-04-03 01:16:35 +0600

    comingsoon button component created

--- src/pages/home/homeComp/features/BentoCard.jsx
+++ src/pages/home/homeComp/features/BentoCard.jsx
@@ -1,24 +1,6 @@
-import { useRef, useState } from "react";
-import { TiLocationArrow } from "react-icons/ti";
+import ComingSoonButton from "./ComingSoonButton";
 
 const BentoCard = ({src, title, description, isComingSoon}) => {
-  const [cursorPosition, setCursorPosition] = useState({x: 0, y: 0});
-  const [hoverOpacity, setHoverOpacity] = useState(0);
-  const hoverButtonRef = useRef(null);
-
-  const handleMouseMove = (event) => {
-    if (!hoverButtonRef.current) return;
-    const rect = hoverButtonRef.current.getBoundingClientRect();
-
-    setCursorPosition({
-      x: event.clientX - rect.left,
-      y: event.clientY - rect.top,
-    });
-  };
-
-  const handleMouseEnter = () => setHoverOpacity(1);
-  const handleMouseLeave = () => setHoverOpacity(0);
-
   return (
     <div className="relative size-full">
       <video src={src} loading="lazy" loop muted autoPlay className="absolute left-0 top-0 size-full object-cover object-center" />
@@ -28,26 +10,7 @@ const BentoCard = ({src, title, description, isComingSoon}) => {
           {description && <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>}
         </div>
 
-        {isComingSoon && (
-          <div
-            ref={hoverButtonRef}
-            onMouseMove={handleMouseMove}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className="border-hsla relative flex w-fit cursor-pointer items-center gap-1 overflow-hidden rounded-full bg-black px-5 py-2 text-xs uppercase text-white/20"
-          >
-            {/* Radial gradient hover effect */}
-            <div
-              className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"
-              style={{
-                opacity: hoverOpacity,
-                background: `radial-gradient(100px circle at ${cursorPosition.x}px ${cursorPosition.y}px, #656fe288, #00000026)`,
-              }}
-            />
-            <TiLocationArrow className="relative z-20" />
-            <p className="relative z-20">coming soon</p>
-          </div>
-        )}
+        {isComingSoon && <ComingSoonButton />}
       </div>
     </div>
   );
---
+++ src/pages/home/homeComp/features/ComingSoonButton.jsx
@@ -0,0 +1,42 @@
+import React, {useRef, useState} from "react";
+import {TiLocationArrow} from "react-icons/ti";
+
+const ComingSoonButton = () => {
+  const [cursorPosition, setCursorPosition] = useState({x: 0, y: 0});
+  const [hoverOpacity, setHoverOpacity] = useState(0);
+  const hoverButtonRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!hoverButtonRef.current) return;
+    const rect = hoverButtonRef.current.getBoundingClientRect();
+
+    setCursorPosition({
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    });
+  };
+
+  const handleMouseEnter = () => setHoverOpacity(1);
+  const handleMouseLeave = () => setHoverOpacity(0);
+  return (
+    <div
+      ref={hoverButtonRef}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="border-hsla relative flex w-fit cursor-pointer items-center gap-1 overflow-hidden rounded-full bg-black px-5 py-2 text-xs uppercase text-white/20"
+    >
+      <div
+        className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"
+        style={{
+          opacity: hoverOpacity,
+          background: `radial-gradient(100px circle at ${cursorPosition.x}px ${cursorPosition.y}px, #656fe288, #00000026)`,
+        }}
+      />
+      <TiLocationArrow className="relative z-20" />
+      <p className="relative z-20">coming soon</p>
+    </div>
+  );
+};
+
+export default ComingSoonButton;
